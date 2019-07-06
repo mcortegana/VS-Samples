d1d0da3d73f4961f4b3ae6f1fcc9c1b533ee5909
Miguel Cortegana <mcortegana93@gmail.com>
2019-07-06 16:58:31 -0500

    Ejemplo log4net

---
+++ Demo_Log4Net/Demo_Log4Net/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Reflection;
+
+namespace Demo_Log4Net
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            try
+            {
+                int zero = 0;
+                int resultado = 5 / zero;
+                logger.Info("El resultado es: " + resultado);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error: ", ex);
+            }
+        }
+    }
+}
